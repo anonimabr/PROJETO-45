5e23a6a1dd7e18c996d2e60d8d110af83074eec5
Anonima <96748986+anonimabr@users.noreply.github.com>
2022-06-06 17:34:57 -0300

    Add files via upload

---
+++ template-projeto-C43-main/sketch.js
@@ -0,0 +1,34 @@
+var gameState
+var vida
+
+var MilhoChorando, MilhoChorando_img
+var MilhoSorrindo, MilhoSorrindo_img
+var MilhoMorto, MilhoMorto_img
+
+var Pássaro, Pássaro_img
+var Porco, Porco_img
+var Lama, Lama_img
+var Elevador, Elevador_img
+var TrituradorMilho, TrituradorMilho_img
+
+function preload() {
+  MilhoChorando_img = loadImage("assets/Milho Chorando.png");
+  MilhoSorrindo_img = loadImage("assets/Milho Sorrindo.png");
+  MilhoMorto_img = loadImage("assets/Milho Morto- Amido.png");
+
+  Pássaro_img = loadImage("assets/Pássaro.png");
+  Porco_img = loadImage("assets/Porco- Inimigo.png");
+  Lama_img = loadImage("assets/Lama- Obstáculo.png");
+  Elevador_img = loadImage("assets/Elevador.png");
+  TrituradorMilho_img = loadImage("assets/Triturador do Milho.png");
+}
+
+function setup() {
+  
+}
+
+function draw() {
+  
+}
+
+
